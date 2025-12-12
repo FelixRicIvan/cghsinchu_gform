b898ebc79808792223315dcd623760327f01725d
FelixRicIvan <felixricardoivan22@gmail.com>
2025-12-13 02:43:05 +0800

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+/*=============== SHOW MENU ===============*/
+
+
+/*=============== REMOVE MENU MOBILE ===============*/
+
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== PARALLAX ===============*/
+let parallax = new Rellax('.parallax');
+
+/*=============== GSAP ANIMATION ===============*/
+gsap.from('.home__letter', 1.2, {opacity: 0, y: 100, delay: .3})
+gsap.from('.home__leaf', 1.2, {opacity: 0, y: 250, delay: .7})
+gsap.from('.home__aog', 1.2, {opacity: 0, y: 200, delay: .8})
+gsap.from('.home__title', 1.2, {opacity: 0, y: -60, delay: 1})
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
